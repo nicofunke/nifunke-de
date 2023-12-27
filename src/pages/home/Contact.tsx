4c3e48833b6c8c8a0503dd8a3e91520d655c1706
nicofunke <funk-e@gross-schneen.de>
2023-12-28 00:50:12 +0100

    feat: contact glass design

--- src/pages/home/Contact.tsx
+++ src/pages/home/Contact.tsx
@@ -1,77 +1,83 @@
 import EmailIcon from "@mui/icons-material/Email";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Divider, IconButton, Stack, Typography } from "@mui/material";
+import { Button, Divider, Stack, Typography } from "@mui/material";
+import { PropsWithChildren } from "react";
 import { Bounce } from "react-awesome-reveal";
 import { Container } from "../../layout/Container";
+
+const ContactBar = ({ children }: PropsWithChildren) => (
+  <Stack
+    direction={"row"}
+    alignItems="stretch"
+    width={{ xs: "100%", sm: "auto" }}
+    minHeight="64px"
+    maxWidth="400px"
+    sx={{
+      backgroundColor: "rgba(255,255,255,0.05)",
+      backdropFilter: "blur(30px)",
+      border: "2px solid rgba(255,255,255,0.03)",
+      borderRadius: "4px",
+    }}
+  >
+    {children}
+  </Stack>
+);
+
+const ContactDivider = () => (
+  <Divider
+    orientation="vertical"
+    flexItem
+    sx={{ bgcolor: "rgba(255,255,255,0.1)" }}
+  />
+);
+
+const ContactButton = ({
+  href,
+  children,
+}: PropsWithChildren<{ href: string }>) => (
+  <Button
+    color="inherit"
+    href={href}
+    target="_blank"
+    sx={{
+      height: "64px",
+      width: "64px",
+      fontSize: "2rem",
+      borderRadius: 0,
+      "&:hover": {
+        backgroundColor: "rgba(200, 157, 184, 0.1)",
+        transition: "background 0.3s ease",
+      },
+    }}
+  >
+    {children}
+  </Button>
+);
 
 const ContactSection = () => {
   return (
     <Container limitedWidth horizontallyCentered>
       <Bounce triggerOnce>
-        <Stack
-          direction={"row"}
-          alignItems="stretch"
-          bgcolor="rgba(255,255,255,0.05)"
-          width={{ xs: "100%", sm: "auto" }}
-          minHeight="64px"
-          borderRadius="4px"
-          maxWidth="400px"
-        >
+        <ContactBar>
           <Typography alignSelf="center" minWidth="140px" paddingX={2}>
             Contact me
           </Typography>
           <Stack direction="row" alignItems="center">
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{ bgcolor: "rgba(255,255,255,0.1)" }}
-            />
-            <IconButton
-              color="inherit"
-              href={"https://www.linkedin.com/in/nicofunke/"}
-              target="_blank"
-              sx={{
-                height: "64px",
-                width: "64px",
-              }}
-            >
+            <ContactDivider />
+            <ContactButton href="https://www.linkedin.com/in/nicofunke/">
               <LinkedInIcon sx={{ fontSize: "2rem" }} />
-            </IconButton>
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{ bgcolor: "rgba(255,255,255,0.1)" }}
-            />
-            <IconButton
-              color="inherit"
-              href={"https://github.com/nicofunke"}
-              target="_blank"
-              sx={{
-                height: "64px",
-                width: "64px",
-              }}
-            >
+            </ContactButton>
+            <ContactDivider />
+            <ContactButton href={"https://github.com/nicofunke"}>
               <GitHubIcon sx={{ fontSize: "2rem" }} />
-            </IconButton>
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{ bgcolor: "rgba(255,255,255,0.1)" }}
-            />
-            <IconButton
-              color="inherit"
-              href={"mailto:contact@nifunke.de"}
-              target="_blank"
-              sx={{
-                height: "64px",
-                width: "64px",
-              }}
-            >
+            </ContactButton>
+            <ContactDivider />
+            <ContactButton href={"mailto:contact@nifunke.de"}>
               <EmailIcon sx={{ fontSize: "2rem" }} />
-            </IconButton>
+            </ContactButton>
           </Stack>
-        </Stack>
+        </ContactBar>
       </Bounce>
     </Container>
   );
